a0a751296f8514d70c70e77b432e5c9fb02d4310
syn <1599386309@qq.com>
2021-04-07 15:48:32 +0800

    init project

---
+++ DouYuZB/DouYuZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DouYuZB
+//
+//  Created by 史雅楠 on 2021/4/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DouYuZB/DouYuZB/DouYuZBApp.swift
@@ -0,0 +1,17 @@
+//
+//  DouYuZBApp.swift
+//  DouYuZB
+//
+//  Created by 史雅楠 on 2021/4/7.
+//
+
+import SwiftUI
+
+@main
+struct DouYuZBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
